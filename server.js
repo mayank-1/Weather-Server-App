ed2d97af4c56d4536335b657b1f9dbb7b9faec2e
mayank-1 <mayankmk165@gmail.com>
2020-01-21 14:43:36 +0530

    Commit 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 9000 || process.env.PORT;
 
-app.use("/", require("./controllers/api/routes"));
+app.get("/", require("./controllers/api/routes"));
 
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
