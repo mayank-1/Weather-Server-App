b70e227f7e7a0fe63445b3c0971c22902dcb01ec
mayank-1 <mayankmk165@gmail.com>
2020-01-21 14:46:42 +0530

    Commit 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 9000 || process.env.PORT;
+const PORT = process.env.PORT || 9000;
 
 app.get("/", require("./controllers/api/routes"));
 
