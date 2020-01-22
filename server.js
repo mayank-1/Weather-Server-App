b487d01f81dd3b469c437bf84269a6edd7fe9839
mayank-1 <mayankmk165@gmail.com>
2020-01-22 11:30:39 +0530

    Commit 10 with cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 9000;
+
+//CORS
+app.use(cors({ credentials: true, origin: true }));
 
 app.use("/", require("./controllers/api/routes"));
 
